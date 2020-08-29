f10762c113cb9b8eca7d4d643e87dd34c525efec
Aman <akrahi21@gmail.com>
2020-08-29 22:49:24 +0530

    some changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express();
 const port=process.env.port||4343;
-app.use("/",express.static(__dirname+"/public"));
+app.use("/",express.static(__dirname+"/dist"));
 app.listen(port,()=>{
     console.log(`Server started at  http://localhost:${port}`);
 })
