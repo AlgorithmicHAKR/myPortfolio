040bce4784d4ac81a1658bb2cb1d0337bdf61e33
Aman <akrahi21@gmail.com>
2021-03-12 01:37:00 +0530

    updated the website

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
    $("#dev").hover(()=>{
        $("#cf").slideIn();
    })
+  $('.work1').slick({dots: true, autoplay: true,
+    autoplaySpeed: 4000,});
+ 
+      
 });
